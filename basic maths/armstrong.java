00f3f40016a35069213e5728fa2d046129e2fd3e
amrith-git-01 <142132572+amrith-git-01@users.noreply.github.com>
2024-11-08 11:44:11 +0530

    Update armstrong.java

--- basic maths/armstrong.java
+++ basic maths/armstrong.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class armstrong {
-  public static void main(String[] args) {
+  public static void main(String[] args) { 
     Scanner scanner = new Scanner(System.in);
     System.out.println("Enter the number: ");
     int num = scanner.nextInt();
