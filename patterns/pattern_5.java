98063c7e604eca6f582414c8c54a0ec36618ee2c
amrith-git-01 <142132572+amrith-git-01@users.noreply.github.com>
2024-11-09 19:59:20 +0530

    Add files via upload

---
+++ patterns/pattern_5.java
@@ -0,0 +1,24 @@
+
+/*
+    * * * * *
+    * * * * 
+    * * *
+    * *
+    * 
+*/
+import java.util.Scanner;
+
+public class pattern_5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of rows: ");
+        int rows = scanner.nextInt();
+        for (int i = rows; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
+}
