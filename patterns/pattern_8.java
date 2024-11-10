d0642df170846c5c882740a5a7d4565453e2c70f
amrith-git-01 <142132572+amrith-git-01@users.noreply.github.com>
2024-11-10 13:04:14 +0530

    Add files via upload

---
+++ patterns/pattern_8.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class pattern_8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of rows: ");
+        int rows = scanner.nextInt();
+        for (int i = 0; i < rows; i++) {
+            // space
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+
+            // stars
+            for (int j = 0; j < 2 * rows - (2 * i + 1); j++) {
+                System.out.print("*");
+            }
+
+            // space
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        scanner.close();
+
+    }
+}
